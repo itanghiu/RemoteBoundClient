1a10080ffb46e9564dc50901eeb4fc183f224378
ihiu <ihiu@docapost.fr>
2016-02-26 10:29:15 +0100

    initial commit

--- app/src/main/java/foryousoft/com/remoteboundclient/ui/MainActivity.java
+++ app/src/main/java/foryousoft/com/remoteboundclient/ui/MainActivity.java
@@ -4,7 +4,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
 import foryousoft.com.remoteboundclient.R;
 import foryousoft.com.remoteboundclient.model.RemoteManager;
 
@@ -16,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     private String TAG = getClass().getName();
     private Button countButton;
     private Button uppercaseButton;
+    private EditText inputEditText;
     private RemoteManager remoteManager;
 
     @Override
@@ -25,19 +28,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         remoteManager = RemoteManager.build(this);
 
+        inputEditText = (EditText) findViewById(R.id.inputText);
         countButton = (Button) findViewById(R.id.countButton);
         uppercaseButton = (Button) findViewById(R.id.uppercaseButton);
 
         countButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                remoteManager.sendMessage("Hello!", COUNT_ACTION);
+                String message = getInputText();
+                remoteManager.sendMessage(message, COUNT_ACTION);
             }
         });
         uppercaseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                remoteManager.sendMessage("Hello!", UPPERCASE_ACTION);
+                String message = getInputText();
+                remoteManager.sendMessage(message, UPPERCASE_ACTION);
             }
         });
     }
@@ -54,5 +60,9 @@ public class MainActivity extends AppCompatActivity {
 
         super.onStart();
         remoteManager.bindToService();
+    }
+
+    public String getInputText() {
+        return inputEditText.getText().toString();
     }
 }
